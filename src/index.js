af259b1812703663f37e7e54e66d688ee7395013
Reviandi Nurulmaqi <fahmikecil16@gmail.com>
2021-10-07 16:08:59 +0800

    Latihan Crud Dasar React JS

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Crud from './crud'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Crud />
   </React.StrictMode>,
   document.getElementById('root')
 );
